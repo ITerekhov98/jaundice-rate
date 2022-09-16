341f275fb22f551b068b480d9d3c1830bd92d4dd
ITerekhov98 <iterekhov98@gmail.com>
2022-09-16 15:06:09 +0300

    Add logging analysis text`s durations

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import aiohttp
 import asyncio
 import os 
+import logging
 from adapters.inosmi_ru import sanitize
 from adapters.exceptions import ArticleNotFound
 from text_tools import check_text_for_jaundicity
@@ -9,16 +10,28 @@ import aiofiles
 from enum import Enum
 from urllib.parse import urlparse
 from async_timeout import timeout
+from contextlib import contextmanager
+import time
 
-TEST_ARTICLES = ['http://inosmi.ru/economic/20190629/245384784.html', 'https://inosmi.ru/politic/20190629/245379332.html', 'https://lenta.ru/brief/2021/08/26/afg_terror/']
+
+TEST_ARTICLES = ['http://inosmi.ru/economic/20190629/245384784.html', 'https://inosmi.ru/politic/20190629/245379332.html', 'https://dvmn.org/media/filer_public/51/83/51830f54-7ec7-4702-847b-c5790ed3724c/gogol_nikolay_taras_bulba_-_bookscafenet.txt']
 URL_FETCH_DELAY = 5
 
+logger = logging.getLogger(__name__)
 
 class ProcessingStatus(Enum):
     OK = 'OK'
     FETCH_ERROR = 'FETCH_ERROR'
     PARSING_ERROR = 'PARSING_ERROR'
     TIMEOUT = 'TIMEOUT'
+
+
+@contextmanager
+def log_duration(logger, url):
+    start = time.monotonic()
+    yield
+    duration = round(time.monotonic() - start, 2)
+    logger.info(f'Анализ статьи {url} закончен за {duration} сек')
 
 
 async def fetch(session, url):
@@ -54,21 +67,25 @@ async def process_article(session, charged_words, url, title, jaundicity_results
     except ArticleNotFound:
         status = ProcessingStatus.PARSING_ERROR.name
     else:
-        sanitized_text = sanitize(html, plaintext=True)
-        rate, article_len = check_text_for_jaundicity(sanitized_text, charged_words)
+        with log_duration(logger, url):
+            sanitized_text = sanitize(html, plaintext=True)
+            rate, article_len = check_text_for_jaundicity(sanitized_text, charged_words)
         status = ProcessingStatus.OK.name
     jaundicity_results.append(
         {
             'Статус': status,
-            'Заголовок:': title,
+            'URL:': title,
             'Рейтинг:': rate,
             'Слов в статье:': article_len
         }
     )
 
 
-
 async def main():
+    logging.basicConfig(
+        format='%(levelname)s : %(message)s',
+        level=logging.INFO
+    )
     jaundicity_results = []
     charged_words = await fetch_charged_words()
     async with aiohttp.ClientSession() as session:
